52da58d10c8ed2785c646dbb19d09202c936180d
ArunpaGit <arun.aswin.nair@gmail.com>
2021-12-05 20:06:40 +0530

    title

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Philips Store
+              e-Store Global
             </span>
           </Link>
         </div>
