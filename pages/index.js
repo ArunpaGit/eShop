e3623fafbfef044b410020e5e39fbc5523c79a1c
Arunkumar <82634170+ArunpaGit@users.noreply.github.com>
2022-01-26 13:40:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,12 @@ export default function Home({ myShops, myTime, mylocale }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>My Shop </title>
+        <title>Philips Shop </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
         <div className="lg:col-span-8 col-span-1">
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
             {myShops.map((shop) => <PostCard shop={shop} key={shop.locale} />)}
           </div>
         </div>
