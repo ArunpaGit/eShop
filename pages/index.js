47d165ce50f949ab54b593a6750bd40f628f273e
Arunkumar <82634170+ArunpaGit@users.noreply.github.com>
2022-11-21 19:43:30 +0100

    Title

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ myShops, myTime, mylocale }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Philips Shop </title>
+        <title>Aruns Shop </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
